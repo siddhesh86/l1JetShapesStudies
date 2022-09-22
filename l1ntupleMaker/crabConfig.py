cb11e0dd17af150dc8eaae806abc15ed53d8b45b
Siddhesh Sawant <sawantsiddhesh08@gmail.com>
2022-09-22 09:31:56 +0200

    l1ntuples for JEC with 12_6_0_pre1 with QCD_pT15to7000

--- l1ntupleMaker/crabConfig.py
+++ l1ntupleMaker/crabConfig.py
@@ -14,17 +14,17 @@ from CRABClient.UserUtilities import config
 config = config()
 
 #config.General.requestName = 
-config.General.workArea = 'L1TNtuple_forHCalL2Calib_PFA1p' 
+config.General.workArea = 'L1TNtuple_forL1JetL2Calib_12_6_0_pre1' 
 config.General.transferOutputs = True
 config.General.transferLogs = False
 
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'mc.py'
+config.JobType.psetName = 'l1ntuple_maker_run3_mc_calib.py'
 #config.JobType.pyCfgParams = ['maxEvt=-1', 'prtEvt=10000', 'nVtxMin=50', 'HCALPFA=%s' % (scheme)] 
 #config.JobType.outputFiles = ['L1Ntuple_HCAL.root']
 
-config.Data.inputDataset = '/QCD_Pt15to7000_TuneCP5_14TeV-pythia8/Run3Summer21DR-FlatPU0to80FEVT_castor_120X_mcRun3_2021_realistic_v6-v1/AODSIM'
-config.Data.secondaryInputDataset = '/QCD_Pt15to7000_TuneCP5_14TeV-pythia8/Run3Summer21DR-FlatPU0to80FEVT_castor_120X_mcRun3_2021_realistic_v6-v1/GEN-SIM-DIGI-RAW'
+config.Data.inputDataset = '/QCD_Pt15to7000_TuneCP5_13p6TeV-pythia8/Run3Winter22DR-L1TPU0to99FEVT_castor_122X_mcRun3_2021_realistic_v9-v2/GEN-SIM-DIGI-RAW' #'/QCD_Pt15to7000_TuneCP5_14TeV-pythia8/Run3Summer21DR-FlatPU0to80FEVT_castor_120X_mcRun3_2021_realistic_v6-v1/AODSIM'
+#config.Data.secondaryInputDataset = '/QCD_Pt15to7000_TuneCP5_14TeV-pythia8/Run3Summer21DR-FlatPU0to80FEVT_castor_120X_mcRun3_2021_realistic_v6-v1/GEN-SIM-DIGI-RAW'
 
 config.General.requestName = config.Data.inputDataset.split('/')[2]
 #config.Data.outputDatasetTag = 'Run3Summer21DR-FlatPU0to80FEVT_castor_120X_mcRun3_2021_realistic_v6-v1_'
@@ -32,8 +32,8 @@ config.Data.outputDatasetTag = '%s_%s' % (config.General.workArea, config.Genera
 
 config.Data.ignoreLocality = False
 config.Data.inputDBS = 'global'
-config.Data.splitting = 'FileBased' # 'Automatic' #'LumiBased' 
-config.Data.unitsPerJob = 1
+config.Data.splitting = 'Automatic' # 'FileBased' # 'Automatic' #'LumiBased' 
+#config.Data.unitsPerJob = 1
 #config.Data.totalUnits = 30
 
 #        config.Data.outLFNDirBase = '/store/user/ssawant/'
